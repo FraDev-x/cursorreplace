558aad154dbf3bc53cab74f5511373a4a10f9ee1
FraDev-x <c11806345@gmail.com>
2025-02-20 18:46:31 +0100

    Update SetCustomCursors.ps1

--- SetCustomCursors.ps1
+++ SetCustomCursors.ps1
@@ -52,11 +52,15 @@ foreach ($key in $customCursors.Keys) {
     $cursorPath = Join-Path $customCursorDir $customCursors[$key]
     Write-Host "Setting $key to $cursorPath..."
     Set-ItemProperty -Path $registryPath -Name $key -Value $cursorPath
+    Set-ItemProperty -Path $registryPath -Name "Scheme Source" -Value 2
 }
 Write-Host "Registry updated with custom cursor paths."
 
 Write-Host "Forcing system to update cursor settings..."
-Start-Process -FilePath "rundll32.exe" -ArgumentList "user32.dll,UpdatePerUserSystemParameters" -NoNewWindow -Wait
+1..3 | ForEach-Object {
+    Start-Process -FilePath "rundll32.exe" -ArgumentList "user32.dll,UpdatePerUserSystemParameters,1,True" -NoNewWindow -Wait
+    Start-Sleep -Milliseconds 500
+}
 Write-Host "System parameters updated via rundll32."
 
 Write-Host "Compiling native method for broadcasting settings change..."
@@ -84,6 +88,11 @@ function Restore-Cursors {
     foreach ($key in $originalCursors.Keys) {
         Write-Host "Restoring $key..."
         Set-ItemProperty -Path $registryPath -Name $key -Value $originalCursors[$key]
+    }
+    Set-ItemProperty -Path $registryPath -Name "Scheme Source" -Value 2
+    1..3 | ForEach-Object {
+        Start-Process -FilePath "rundll32.exe" -ArgumentList "user32.dll,UpdatePerUserSystemParameters,1,True" -NoNewWindow -Wait
+        Start-Sleep -Milliseconds 500
     }
     [NativeMethods]::SendMessageTimeout([NativeMethods]::HWND_BROADCAST, [NativeMethods]::WM_SETTINGCHANGE, [IntPtr]::Zero, "Control Panel", 0, 100, [ref]$result)
     Write-Host "Original cursors restored."
